6b600659e4f432b4e6366e5652c6ba156b1ba0ed
Lucas Roman <lucas.e.roman@gmail.com>
2022-04-18 22:09:34 -0700

    added script for appending weather info for five day forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,6 +8,37 @@ var wind = document.getElementById("wind")
 var humidity = document.getElementById("humidity")
 var uvIndex = document.getElementById("uv-index")
 
+var fiveDay1header = document.getElementById("header1")
+var fiveDay1Icon = document.getElementById("weatherIcon1")
+var fiveDay1Temp = document.getElementById("temp1")
+var fiveDay1Wind = document.getElementById("wind1")
+var fiveDay1Humidity = document.getElementById("humidity1")
+
+var fiveDay2header = document.getElementById("header2")
+var fiveDay2Icon = document.getElementById("weatherIcon2")
+var fiveDay2Temp = document.getElementById("temp2")
+var fiveDay2Wind = document.getElementById("wind2")
+var fiveDay2Humidity = document.getElementById("humidity2")
+
+var fiveDay3header = document.getElementById("header3")
+var fiveDay3Icon = document.getElementById("weatherIcon3")
+var fiveDay3Temp = document.getElementById("temp3")
+var fiveDay3Wind = document.getElementById("wind3")
+var fiveDay3Humidity = document.getElementById("humidity3")
+
+var fiveDay4header = document.getElementById("header4")
+var fiveDay4Icon = document.getElementById("weatherIcon4")
+var fiveDay4Temp = document.getElementById("temp4")
+var fiveDay4Wind = document.getElementById("wind4")
+var fiveDay4Humidity = document.getElementById("humidity4")
+
+var fiveDay5header = document.getElementById("header5")
+var fiveDay5Icon = document.getElementById("weatherIcon5")
+var fiveDay5Temp = document.getElementById("temp5")
+var fiveDay5Wind = document.getElementById("wind5")
+var fiveDay5Humidity = document.getElementById("humidity5")
+
+
 function getAPI() {
     var city = document.getElementById("searchBar").value;
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIkey;
@@ -23,11 +54,41 @@ function getAPI() {
                     return response.json();
                 }) .then(function (data) {
                     console.log(data)
-                    temperature.innerText= "Current Temperature: " + data.current.temp
-                    cityDate.innerText="Today's Date: " + moment.unix(data.current.dt).format("MMM Do YY")
-                    wind.innerText="Current Wind Speed: " + data.current.wind_speed
-                    humidity.innerText="Current Humidity: " + data.current.humidity
-                    uvIndex.innerText="Current Uv-Index: " + data.current.uvi
+                    temperature.innerText= "  Current Temperature: " + data.current.temp + " F";
+                    cityDate.innerText="  Today's Date: " + moment.unix(data.current.dt).format("MMM Do YY");
+                    wind.innerText="  Current Wind Speed: " + data.current.wind_speed + " mph";
+                    humidity.innerText="  Current Humidity: " + data.current.humidity + " %";
+                    uvIndex.innerText="  Current Uv-Index: " + data.current.uvi;
+
+                    fiveDay1header.innerText= moment.unix(data.daily[1].dt).format("MMM Do YY");
+                    // fiveDay1Icon.innerText= data.daily[1].weather[0]
+                    fiveDay1Temp.innerText= "Temp:   " + data.daily[1].temp.day + " F";
+                    fiveDay1Wind.innerText= "WS:   " + data.daily[1].wind_speed + " mph";
+                    fiveDay1Humidity.innerText= "HMD:   " + data.daily[1].humidity + " %" ;
+
+                    fiveDay2header.innerText= moment.unix(data.daily[2].dt).format("MMM Do YY");
+                    // fiveDay1Icon.innerText= data.daily[1].weather[0]
+                    fiveDay2Temp.innerText= "Temp:   " + data.daily[2].temp.day + " F";
+                    fiveDay2Wind.innerText= "WS:   " + data.daily[2].wind_speed + " mph";
+                    fiveDay2Humidity.innerText= "HMD:   " + data.daily[2].humidity + " %"; 
+
+                    fiveDay3header.innerText= moment.unix(data.daily[3].dt).format("MMM Do YY");
+                    // fiveDay1Icon.innerText= data.daily[1].weather[0]
+                    fiveDay3Temp.innerText= "Temp:   " + data.daily[3].temp.day + " F";
+                    fiveDay3Wind.innerText= "WS:   " + data.daily[3].wind_speed + " mph";
+                    fiveDay3Humidity.innerText= "HMD:   " + data.daily[3].humidity + " %"; 
+
+                    fiveDay4header.innerText= moment.unix(data.daily[4].dt).format("MMM Do YY");
+                    // fiveDay1Icon.innerText= data.daily[1].weather[0]
+                    fiveDay4Temp.innerText= "Temp:   " + data.daily[4].temp.day + " F";
+                    fiveDay4Wind.innerText= "WS:   " + data.daily[4].wind_speed + " mph";
+                    fiveDay4Humidity.innerText= "HMD:   " + data.daily[4].humidity + " %";
+
+                    fiveDay5header.innerText= moment.unix(data.daily[5].dt).format("MMM Do YY");
+                    // fiveDay1Icon.innerText= data.daily[1].weather[0]
+                    fiveDay5Temp.innerText= "Temp:   " + data.daily[5].temp.day + " F";
+                    fiveDay5Wind.innerText= "WS:   " + data.daily[5].wind_speed + " mph";
+                    fiveDay5Humidity.innerText= "HMD:   " + data.daily[5].humidity + " %";
 
                 })
                 // temp.innerText=data.main.temp
